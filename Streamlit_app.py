7f7d3273ee924d8263779dc231ee1e54dad3fe0f
Shan575103 <147678552+Shan575103@users.noreply.github.com>
2023-10-11 22:43:05 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,4 +5,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+🍌🥭 Build Your Own Fruit Smoothie 🥝🍇
 
